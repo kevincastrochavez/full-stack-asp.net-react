e4476ed6113696dbd6680c2fbfd29b0e79aca51f
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-05-20 12:26:22 -0600

    added interfaces for types

---
+++ PizzaClient/src/types.ts
@@ -0,0 +1,14 @@
+export interface Pizza {
+  id: number;
+  name: string;
+  description: string;
+}
+
+export interface PizzaItemProps {
+  name: string;
+  data: Pizza[];
+  onCreate: (item: Pizza) => void;
+  onUpdate: (item: Pizza) => void;
+  onDelete: (id: number) => void;
+  error?: string;
+}
